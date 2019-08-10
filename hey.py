9dd3fc4ef0bf79a0b6f6d24358d925bffb766b10
Srijan-G <srijangupta27@gmail.com>
2019-08-10 13:32:30 +0530

    Add existing files

---
+++ hey.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+filexml=open("task.xml","r")
+page_name=input()
+frame_name=input()
+soup=BeautifulSoup(filexml,"lxml-xml")
+tag=soup.find("Page",{"name":page_name})
+a=tag.find("step",{"frame":frame_name})
+for i in (a["pose"].split()):
+    if i[0]!='-':
+        i=" "+i
+    print(i)
+
